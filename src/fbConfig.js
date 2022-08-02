d2944f894fb7d8d19cbc679d88de0bb76a8988a4
nehaejaz <nehaejaz29@gmail.com>
2022-08-02 02:08:20 -0400

    fixed imports

--- src/fbConfig.js
+++ src/fbConfig.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getStorage, ref} from 'firebase/storage'
+import {getStorage} from 'firebase/storage'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +17,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const storage = getStorage(app)
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app)
+
+export { app, storage};
+
